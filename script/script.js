8bc0127c527e4e2e364449fa4160e17de9d34084
Marcos Andrade <82156020+Marcos-NineNine@users.noreply.github.com>
2021-04-13 10:54:17 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function logar(){ //fazendo uma função	
+	alert(document.getElementById("cLogin").value + 
+	" você foi conectado com sucesso!!");
+
+}
+
+function cadastrar(){ //fazendo uma função	
+	alert("Cadastrado com sucesso!!");
+	
+	alert("Bem Vindo " + document.getElementById("cNome").value + " !!");
+}
+
+function sugestao(){ //fazendo uma função	
+	alert("Sua sugestão foi para a análise.");
+}
